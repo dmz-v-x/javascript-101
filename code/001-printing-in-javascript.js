5a24dae0564f14d8993ba6115bb26e2684d21979
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-04-05 16:13:25 +0530

    Create 001-printing-in-javascript.js

---
+++ code/001-printing-in-javascript.js
@@ -0,0 +1,56 @@
+console.log("Hello World");
+
+// Printing numbers and variables
+
+let age = 25;
+console.log(age);
+
+let name = "Himanshu"
+console.log(name);
+
+// Printing multiple values (console.log: automatically print space-separated values )
+
+let city = "Bangalore"
+let year = 2026
+
+console.log(city, year);
+
+// Using String concatenation
+
+console.log("Hello " + "World");
+console.log("Age: "+ 25);
+
+// Using Template Literal
+
+let name = "Ravi"
+let age = 22;
+
+console.log(`My name is ${name} and I am ${age} years old`);
+
+// Using newLine
+
+console.log("Line 1\nLine 2\nLine 3");
+
+// Template Literal with line breaks
+
+console.log(`
+Line 1
+Line 2
+Line 3
+`);
+
+// Printing arrays and objects
+
+let numbers = [1, 2, 3, 4]
+console.log(numbers);
+
+let user = {
+  name: "Asha",
+  age: 25
+}
+
+console.log(user);
+
+// Show a pop-up message
+
+alert("Hello");
